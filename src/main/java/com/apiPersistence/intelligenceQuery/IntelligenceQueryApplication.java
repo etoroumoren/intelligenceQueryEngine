670afc7abf3963d438d8fd724ad903d602ccae11
Etoro Umoren <torohkiss@yahoo.com>
2026-04-21 14:12:53 +0100

    My first commit. Created application properties

---
+++ src/main/java/com/apiPersistence/intelligenceQuery/IntelligenceQueryApplication.java
@@ -0,0 +1,13 @@
+package com.apiPersistence.intelligenceQuery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligenceQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligenceQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiPersistence/intelligenceQuery/IntelligenceQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiPersistence.intelligenceQuery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligenceQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
